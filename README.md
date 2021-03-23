c8c4f9dbd7aaf06d7d03c2fb0795f3da9860cc1d
Bugra <ordabiadamvar@gmail.com>
2021-03-23 19:18:41 +0300

    main: Adds: Dual Range Slider Basic - Simple

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# dual-range-slider
+# Dual Range Slider
+
+This is simple JavaScript Class for Dual Range Slider. Easy to use, easy to install, easy setup.
+
+# How it works?
+
+`npm install dual-range-slider`
+
+Create an HTML Markup
+
+`
+    <div class="dual-range" data-min="20" data-max="70">
+		<span class="handle left"></span>
+		<span class="highlight"></span>
+		<span class="handle right"></span>
+	</div>
+`
+
+then add CSS (SCSS) code to your styles
+
+    .dual-range {
+        --range-size:20px;
+        --range-width:150px;
+        --handle-size:1.3;
+        height:var(--range-size);
+        width:var(--range-width);
+        background:var(--clr-box-dark);
+        border-radius:50px;
+        position:relative;
+        user-select:none;
+        
+        .highlight {
+            position:absolute;
+            height:var(--range-size);
+            width:calc(calc(var(--x-2) - var(--x-1)) + calc(var(--range-size) * var(--handle-size)));
+            left:var(--x-1);
+            background:var(--clr-prim);
+            z-index:1;
+            border-radius:50px;
+        }
+        
+        .handle {
+            width:calc(var(--range-size) * var(--handle-size));
+            height:calc(var(--range-size) * var(--handle-size));
+            background:#fff;
+            position:absolute;
+            box-shadow:var(--shadow);
+            border-radius:50%;
+            top:50%;
+            transform:translateY(-50%);
+            z-index:2;
+            cursor:grab;
+            &:active {
+                cursor:grabbing;
+            }
+            
+            &.left {
+                left:var(--x-1);
+            }
+            
+            &.right {
+                left:var(--x-2);
+            }
+            
+            &::after {
+                content:'$'attr(data-value);
+                position:absolute;
+                top:100%;
+                left:50%;
+                transform:translateX(-50%);
+            }
+        }
+        
+	}
+	
---
+++ index.js
@@ -0,0 +1,101 @@
+class dualRangeSlider {
+    constructor(rangeElement) {
+        this.range = rangeElement
+        this.min = Number(rangeElement.dataset.min)
+        this.max = Number(rangeElement.dataset.max)
+        this.startPos = 0
+        this.activeHandle
+        this.range.innerHTML = `<outputs><min></min><max></max></outputs><controls><handle class="left"></handle><highlight></highlight><handle class="right"></handle></controls>`
+        this.handles = [...this.range.querySelectorAll('handle')]
+        this.min_label = this.range.querySelector('min')
+        this.max_label = this.range.querySelector('max')
+
+        this.handles.forEach(handle => {
+            handle.addEventListener('mousedown', this.startMove.bind(this))
+            handle.addEventListener('touchstart', this.startMoveTouch.bind(this))
+        })
+
+        window.addEventListener('mouseup', this.stopMove.bind(this))
+        window.addEventListener("touchend", this.stopMove.bind(this))
+        window.addEventListener("touchcancel", this.stopMove.bind(this))
+        window.addEventListener("touchleave", this.stopMove.bind(this))
+
+        const rangeRect = this.range.getBoundingClientRect()
+        const handleRect = this.handles[0].getBoundingClientRect()
+        this.range.style.setProperty("--x-1", - handleRect.width/2 + "px")
+        this.range.style.setProperty("--x-2", rangeRect.width - handleRect.width/2 + "px")
+        this.handles[0].dataset.value = this.range.dataset.min
+        this.handles[1].dataset.value = this.range.dataset.max
+        this.updateLabels()
+    }
+
+    startMoveTouch(e) {
+        const handleRect = e.target.getBoundingClientRect()
+        this.startPos = e.touches[0].clientX - handleRect.x;
+        this.activeHandle = e.target;
+        this.moveTouchListener = this.moveTouch.bind(this)
+        window.addEventListener("touchmove", this.moveTouchListener);
+    }
+
+    startMove(e) {
+        this.startPos = e.offsetX;
+        this.activeHandle = e.target;
+        this.moveListener = this.move.bind(this)
+        window.addEventListener("mousemove", this.moveListener);
+    }
+
+    moveTouch(e) {
+        this.move({clientX: e.touches[0].clientX})
+    }
+
+    move(e) {
+        const isLeft = this.activeHandle.classList.contains("left")
+        const property = isLeft ? "--x-1" : "--x-2";
+        const parentRect = this.range.getBoundingClientRect();
+        const handleRect = this.activeHandle.getBoundingClientRect();
+        let newX = e.clientX - parentRect.x - this.startPos;
+        if(isLeft) {
+            const otherX = parseInt(this.range.style.getPropertyValue("--x-2"));
+            newX = Math.min(newX, otherX - handleRect.width)
+            newX = Math.max(newX, 0 - handleRect.width/2)
+        } else {
+            const otherX = parseInt(this.range.style.getPropertyValue("--x-1"));
+            newX = Math.max(newX, otherX + handleRect.width)
+            newX = Math.min(newX, parentRect.width - handleRect.width/2)
+        }
+        this.activeHandle.dataset.value = this.calcHandleValue((newX + handleRect.width/2) / parentRect.width)
+        this.range.style.setProperty(property, newX + "px");
+        this.updateLabels()
+    }
+
+    updateLabels(e) {
+        this.min_label.innerHTML = this.range.dataset.minLabel + this.handles[0].dataset.value
+        this.max_label.innerHTML = this.range.dataset.maxLabel + this.handles[1].dataset.value
+    }
+
+    calcHandleValue(percentage) {
+        return Math.round(percentage * (this.max - this.min) + this.min)
+    }
+
+    stopMove(evt) {
+        window.removeEventListener("mousemove", this.moveListener);
+        window.removeEventListener("touchmove", this.moveTouchListener);
+
+        const event = new CustomEvent('dualRangeChanged', {
+            detail: {
+                min: this.handles[0].dataset.value,
+                max: this.handles[1].dataset.value
+            }
+        })
+
+        if(this.range.contains(evt.target) || evt.target === this.range) {
+            document.dispatchEvent(event)
+        }
+    }
+}
+
+document.addEventListener('dualRangeChanged', function(evt) {
+    console.log(evt.detail.min, evt.detail.max)
+});
+
+module.exports = dualRangeSlider;
